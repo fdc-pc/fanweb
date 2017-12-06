552ddca61ae071c2b26180cab82c0b8b985c9b2d
Fandc <fandc@titans-ele.com>
2017-12-06 15:05:04 +0800

    vip
    视频破解

---
+++ VIP/foot.php
@@ -0,0 +1,16 @@
+<?php
+//版权：范德澄（修改）
+//作者：范德澄
+//网址：qds171636273.my3w.com
+
+?>
+<div class="panel-body" style="text-align: center;">
+ <div class="container-fluid">
+  <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=457343315&site=qq&menu=yes" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-thumbs-up"></span>不会用点我</a>
+  <a target="_blank" href="#" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-globe"></span>更多接口后续更新</a> 
+ <!--<button type="button" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-user"></span>  <a href="http://www.zzidc.com/main/huodong/registspread/spreadid_80394_hdName_regist2016.html">1元主机支持</a>
+ </button> -->
+<br><span class="copyright">Copyright &copy; 2017 qds171636273.my3w.com 范德澄  <a href="" target="_blank" title="范德澄"> <?php echo $ini['title']?> </a> - Collect from <a href="qds171636273.my3w.com" title="范德澄" target="_blank">开发者</a></span>
+</div>
+</body>
+</html>
